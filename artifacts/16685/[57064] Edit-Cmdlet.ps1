755d8c0c4f01bd6b2889f662dc52244d1976d278
Ben Wilkinson <benwilk@microsoft.com>
2021-04-01 07:19:50 -0700

    update edit-cmdlet

--- artifacts/16685/[57064] Edit-Cmdlet.ps1
+++ artifacts/16685/[57064] Edit-Cmdlet.ps1
@@ -1,24 +1,32 @@
-﻿function Edit-Cmdlet {
-param 
+﻿function Edit-Cmdlet
+{
+    param 
     (
-        [String]$Cmdlet
+        [String]$Cmdlet = 'Trace-AzureRMDSCExtension'
     )
 
-try {
-
-        $Base = Get-Command -Name $Cmdlet -ErrorAction Stop  | 
-                Select-Object Module | 
-                ForEach-Object {
-                    Get-Module $_.Module -ErrorAction Stop | 
-                    Select-Object -ExpandProperty ModuleBase
-                    }
-        $Path = (Join-Path -Path $Base -ChildPath ($Cmdlet + ".*") -Resolve -ErrorAction Stop) 
-        psedit -filenames $Path
-    }
-catch
+    try
     {
-        "Cannot find that file, this is likely not a user written cmdlet"
-    }
 
+        $Base = Get-Command -Name $Cmdlet -ErrorAction Stop -ov command |
+            Select-Object Module |
+            ForEach-Object {
+                Get-Module $_.Module -ErrorAction Stop |
+                    Select-Object -ExpandProperty ModuleBase
+                }
+        $Path = (Join-Path -Path $Base -ChildPath ($Command[0].Name + '.*') -Resolve -ErrorAction Stop) 
+        if ($host.name -like '*ise*' -or $host.name -eq 'Visual Studio Code Host')
+        {
+            psEdit $Path
+        }
+        else
+        {
+            code $Path
+        }
+    }
+    catch
+    {
+        'Cannot find that file, this is likely not a user written cmdlet'
+    }
 }#Edit-Cmdlet
 
