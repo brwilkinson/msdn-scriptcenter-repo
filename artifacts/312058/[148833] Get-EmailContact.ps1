5166feae54b3bebfaf6f2d3c42ab8842b01511ac
Ben Wilkinson <benwilk@microsoft.com>
2021-12-06 16:07:05 -0800

    update send-email script to remove final >

--- artifacts/312058/[148833] Get-EmailContact.ps1
+++ artifacts/312058/[148833] Get-EmailContact.ps1
@@ -1,19 +1,18 @@
-﻿function Get-EmailContact {
-  Param (
-    [String[]]$result = " <John.Hall@contoso.com>; Jones, Jamoe <Jamie.Jones@contoso.com>; Ralphe, Sarah <Sarah.Ralphe@contoso.com>; Thames, Aaron <Aaron.Thames@contoso.com>; Burows, Michaele <Michaele.Burows@contoso.com>"
-  )
+﻿function Get-EmailContact
+{
+    Param (
+        [String]$result = '<John.Hall@contoso.com>; Jones, Jamoe <Jamie.Jones@contoso.com>; Ralphe, Sarah <Sarah.Ralphe@contoso.com>; Thames, Aaron <Aaron.Thames@contoso.com>; Burows, Michaele <Michaele.Burows@contoso.com>'
+    )
   
   
-  $result -split ";" | foreach {
+    $result -split ';' | ForEach-Object {
     
-    $Name,$Email = $_.trimend(">") -split " <"
-    $hash = @{
-        Name  = $Name.trim()
-        Email = $Email.trim()
-
-
+        $Name, $Email = $_.trimend('>\s*') -split '\s*<'
+        $hash = @{
+            Name  = $Name.trim()
+            Email = $Email.trim()
+        }
+    
+        New-Object -TypeName psobject -Property $hash | Select-Object Name, Email
     }
-    New-Object -TypeName psobject -Property $hash | Select Name,Email
-  }
-  
 }
